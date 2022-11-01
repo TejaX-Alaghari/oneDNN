7a3d308ac3c9548d7a21ef689e461af7b381428e
Teja Alaghari <tejax.alaghari@intel.com>
2022-11-01 13:06:19 +0000

    Addiing initial configuration ffor conv

--- src/gpu/amd/sycl_hip_engine.cpp
+++ src/gpu/amd/sycl_hip_engine.cpp
@@ -23,6 +23,7 @@
 #include "sycl/sycl_utils.hpp"
 
 #include "gpu/amd/miopen_binary.hpp"
+#include "gpu/amd/miopen_convolution.hpp"
 #include "gpu/amd/miopen_eltwise.hpp"
 #include "gpu/amd/miopen_lrn.hpp"
 #include "gpu/amd/miopen_pooling.hpp"
@@ -142,6 +143,10 @@ constexpr dnnl::impl::impl_list_item_t sycl_hip_impl_list[] = {
         INSTANCE(miopen_pooling_bwd_t)
         // Reduction
         INSTANCE(miopen_reduction_t)
+        //Convolution
+        INSTANCE(miopen_convolution_fwd_t)
+        INSTANCE(miopen_convolution_bwd_data_t)
+        INSTANCE(miopen_convolution_bwd_weights_t)
         nullptr,
 };
 // clang-format on
